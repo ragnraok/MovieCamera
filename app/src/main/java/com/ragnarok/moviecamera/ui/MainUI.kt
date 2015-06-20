79618edab44a695bbac397726b7507f566560e8e
ragnraok <okone1288@gmail.com>
2015-06-20 23:58:59 +0800

    add intro animation for create button

--- app/src/main/java/com/ragnarok/moviecamera/ui/MainUI.kt
+++ app/src/main/java/com/ragnarok/moviecamera/ui/MainUI.kt
@@ -3,6 +3,9 @@ package com.ragnarok.moviecamera.ui
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.animation.OvershootInterpolator
+import android.widget.Button
+import android.widget.ImageButton
 import com.ragnarok.moviecamera.R
 
 /**
@@ -16,10 +19,16 @@ class MainUI: BaseUI() {
     var imageList: RecyclerView? = null
     var imageListAdapter: ImageListAdapter? = null
     var imageLayoutManager: LinearLayoutManager? = null
+    var createButton: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        createButton = findViewById(R.id.create) as ImageButton
+        
+        val createButtonTransY = getResources().getDimension(R.dimen.fab_size) + getResources().getDimension(R.dimen.fab_bottom_margin) + 100
+        createButton?.setTranslationY(createButtonTransY)
         
         initImageList()
     }
@@ -35,5 +44,7 @@ class MainUI: BaseUI() {
 
     override fun onToolbarInitAnimFinish() {
         imageListAdapter?.updateItems()
+        
+        createButton?.animate()!!.translationY(0f).setInterpolator(OvershootInterpolator()).setDuration(300).setStartDelay(250)
     }
 }
